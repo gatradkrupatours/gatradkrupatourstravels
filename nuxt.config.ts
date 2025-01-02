e1541d58e31b1fe6ed32a4ece3939b20235cb64c
Juned Khatri <juned.khatri31@gmail.com>
2025-01-02 13:38:51 +0530

    Add js in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        { name: 'description', content: 'Everything about Nuxt 3' }
+      ],
+      script: [
+        { src: '/js/jquery.js' },
+        { src: '/js/bootstrap.min.js' },
+        { src: '/js/owl.carousel.min.js' },
+        { src: '/js/smoothscroll.js' },
+        { src: '/js/custom.js' },
+        { src: '/js/contact.js' },
+      ]
+    }
+  },
 })
