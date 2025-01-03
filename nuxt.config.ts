582e124ed362dbb4bb0ef5ed08338c7b3333a1f9
Juned Khatri <juned.khatri31@gmail.com>
2025-01-03 12:39:54 +0530

    Disable error on failure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-01-02',
+  compatibilityDate: "2025-01-02",
   runtimeConfig: {
     public: {
       gtm: {
-        id: process.env.NODE_ENV === 'production' ? 'G-4Q3N91R954' : 'G-4K4Q0S4G7N'
+        id:
+          process.env.NODE_ENV === "production"
+            ? "G-4Q3N91R954"
+            : "G-4K4Q0S4G7N",
       },
-    }
+    },
   },
-})
+  nitro: {
+    prerender: {
+      failOnError: false,
+    },
+  },
+});
